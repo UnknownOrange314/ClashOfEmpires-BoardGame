d1e8bf5b46d2fb3909ce33b434c4e5354ccebd04
bponnaluri <bponnaluri@gmail.com>
2017-11-27 18:58:55 -0500

    Added more unit tests for Territory class

--- src/model/Territory.kt
+++ src/model/Territory.kt
@@ -2,11 +2,40 @@ package model
 
 class Territory(){
 
+    var troops = ArrayList<Troop>();
+    var borders = HashSet<Territory>();
+
+    var owner:Player
+        get() = owner
+        set(player) {
+            owner = player
+        }
     fun getNumTroops():Int {
-        return 0;
+        return troops.size;
     }
 
-    fun addTroop(t: Troop) {}
+    fun addTroop(troop: Troop) {
+        troops.add(troop);
+    }
+
+    fun removeTroop() {
+        if (!troops.isEmpty()) {
+            troops.removeAt(troops.size - 1);
+        }
+    }
+
+    fun addBorder(territory: Territory) {
+        borders.add(territory);
+    }
+
+    fun isBorder(territory: Territory): Boolean {
+        return borders.contains(territory);
+    }
+
+    fun numBorders(): Int {
+        return borders.size;
+    }
+
 
 }
 
--- src/test/model/TerritoryTest.kt
+++ src/test/model/TerritoryTest.kt
@@ -4,6 +4,8 @@ import model.Territory
 import model.Troop
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 
 class TerritoryTest {
@@ -15,10 +17,84 @@ class TerritoryTest {
     }
 
     @Test
-    fun testAddTroop() {
+    fun testNewTerritoryHasNoTroops() {
+        val territory = setup();
+        assertEquals(territory.getNumTroops(), 0);
+    }
+
+    @Test
+    fun testAddOneTroop_CorrectNumberOfTroops() {
         val territory = setup();
         territory.addTroop(Troop.createTroop())
         assertEquals(territory.getNumTroops(), 1);
     }
 
+    @Test
+    fun testAddMultipeTroops_CorrectNumberOfTroops() {
+        val territory = setup();
+        val numTroops = 100;
+        for (i in 1..numTroops) {
+            territory.addTroop(Troop.createTroop());
+        }
+        assertEquals(territory.getNumTroops(), numTroops);
+    }
+
+    @Test
+    fun testAddOneTroop_AndRemoveOneTroop_EmptyTerritory() {
+        val territory = setup();
+        territory.addTroop(Troop.createTroop());
+        territory.removeTroop();
+        assertEquals(territory.getNumTroops(), 0);
+
+    }
+
+    @Test
+    fun testRemoveTroopFromEmptyTerritory_DoesNothing() {
+        val territory = setup();
+        territory.removeTroop();
+        assertEquals(territory.getNumTroops(), 0);
+    }
+
+    fun testNewTerritory_NoBorder() {
+        val brittany = Territory();
+        assertEquals(brittany.numBorders(), 0);
+    }
+    @Test
+    fun testDistantTerritories_NoBorder() {
+        val catalonia = Territory();
+        val prussia = Territory();
+        assertFalse(catalonia.isBorder(prussia));
+    }
+
+    @Test
+    fun testAddSingleTerritoryBorder_savesBorder() {
+        val madrid = Territory();
+        val toledo = Territory();
+        madrid.addBorder(toledo);
+        toledo.addBorder(madrid);
+        assertTrue(madrid.isBorder(toledo));
+        assertTrue(toledo.isBorder(madrid));
+    }
+
+    @Test
+    fun testAddSameBorderTwice_createsOnlyOneBorder() {
+        val madrid = Territory();
+        val toledo = Territory();
+        madrid.addBorder(toledo);
+        madrid.addBorder(toledo);
+        assertEquals(madrid.numBorders(), 1);
+    }
+    @Test
+    fun testAddMultipleTerritoryBorders_savesAllBorders() {
+        val cordoba = Territory();
+        val seville = Territory();
+        val granada = Territory();
+
+        cordoba.addBorder(seville);
+        cordoba.addBorder(granada);
+
+        assertTrue(cordoba.isBorder(seville));
+        assertTrue(cordoba.isBorder(granada));
+        assertEquals(cordoba.numBorders(), 2);
+    }
 }
